0e62901d2fcbd756cf462de45543b7858cc31637
Zdenek L <luzkix@seznam.cz>
2020-07-24 19:02:35 +0200

    Added excercise DefineBasicInfo

---
+++ week-2/day-3-JavaExpressionsFlow/src/DefineBasicInfo.java
@@ -0,0 +1,15 @@
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        String name = "Zdenek L.";
+        int age = 37;
+        double height = 1.91;
+        boolean marriage = false;
+        System.out.println("My name is: "+ name + "\n"
+                + "My age is: " + age + "\n"
+                + "My height is: " + height + "\n"
+                + "My marriage status is: " + marriage +"\n"
+                + "Hope this info stay safe with you guys! :-)"
+        );
+    }
+
+}
