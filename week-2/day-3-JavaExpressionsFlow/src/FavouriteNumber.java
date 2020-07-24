3239e3100c3eedfdbe7afae5ec472b5916e09844
Zdenek L <luzkix@seznam.cz>
2020-07-24 17:42:45 +0200

    Added excercise FavouriteNumber

---
+++ week-2/day-3-JavaExpressionsFlow/src/FavouriteNumber.java
@@ -0,0 +1,7 @@
+public class FavouriteNumber {
+    public static void main(String[] args) {
+        int myFavouriteNumber = 3;
+        System.out.println("My favourite number is: " + myFavouriteNumber);
+
+    }
+}
