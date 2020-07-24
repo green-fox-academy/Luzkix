8968f384e5766b1d76101b36b996ebdcb8638fef
Zdenek L <luzkix@seznam.cz>
2020-07-24 15:23:03 +0200

    IntroduceYourself excercise

--- week-2/day-3-JavaExpressionsFlow/src/IntroduceYourself.java
+++ week-2/day-3-JavaExpressionsFlow/src/IntroduceYourself.java
@@ -1,2 +1,12 @@
 public class IntroduceYourself {
+    public static void main(String[] args) {
+        System.out.println("My name is Zdenek Luzar\n" +
+                "My age is: " + 37 + "\n" +
+                "My Height is: " + 197 + "cm");
+        System.out.println("Or in less complicated way:");
+        System.out.println("My name is Zdenek");
+        System.out.println(31);
+        System.out.println(1.87);
+
+    }
 }
