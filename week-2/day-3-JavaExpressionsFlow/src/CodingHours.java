abcfd7884c4e13b11840d1afe7e966677f5ebc87
Zdenek L <luzkix@seznam.cz>
2020-07-24 16:59:55 +0200

    Added excercise CodingHours

--- week-2/day-3-JavaExpressionsFlow/src/CodingHours.java
+++ week-2/day-3-JavaExpressionsFlow/src/CodingHours.java
@@ -1,2 +1,12 @@
 public class CodingHours {
+    public static void main(String[] args) {
+        int codingWeekWorkdays = 5;
+        int codingHoursOfStudent = 6;
+        int semesterOfStudent = 17;
+        int studentHours = codingHoursOfStudent * codingWeekWorkdays * semesterOfStudent;
+        int standardHours = 52 * semesterOfStudent;
+
+        System.out.println("Coding hours of student during semester: " + studentHours);
+        System.out.println("Average % of coding hours in the semester is: " + ((float)studentHours / standardHours * 100) + "%");
+    }
 }
