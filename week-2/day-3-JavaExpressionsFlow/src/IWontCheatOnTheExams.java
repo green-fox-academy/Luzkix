3583ca8b3adc8e8d51bcc5311f80a3abecf48be6
Zdenek L <luzkix@seznam.cz>
2020-07-25 14:33:26 +0200

    Added excercise IWontCheatOnTheExams

---
+++ week-2/day-3-JavaExpressionsFlow/src/IWontCheatOnTheExams.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class IWontCheatOnTheExams {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        for (int a = 0; a < 100; a++) {
+            System.out.println("I won't cheat on the exam!");
+        }
+    }
+}
