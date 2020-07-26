75cebba33d26bfab3c7cd2fc66ec85299bdb5d74
Zdenek L <luzkix@seznam.cz>
2020-07-26 21:35:07 +0200

    GuessTheNumber excercise - very funny program!!

---
+++ week-2/day-3-JavaExpressionsFlow/src/GuessTheNumber.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class GuessTheNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Tell mi the number! Zadej číslo!");
+        int definedNumber = scanner.nextInt();
+
+        System.out.println("Guess the number! / Hádej číslo!:");
+        int guessedNumber = scanner.nextInt();
+
+        //toto if jsem vložil až nakonec, protože když by se číslo rovnou uhádlo, program skončí a nevypíše žádnou větu.
+        if (guessedNumber == definedNumber) {
+            System.out.println("You found the number: / Správně! Číslo je: " + definedNumber);
+        }
+
+        while (guessedNumber != definedNumber) {
+            if (guessedNumber < definedNumber) {
+                System.out.println("The stored number is higher / Hádej vyšší číslo!");
+                guessedNumber = scanner.nextInt();
+            }
+            if (guessedNumber > definedNumber) {
+                System.out.println("The stored number is lower / Hádej menší číslo!");
+                guessedNumber = scanner.nextInt();
+            }
+            if (guessedNumber == definedNumber) {
+                System.out.println("You found the number: / Správně! Číslo je: " + definedNumber);
+            }
+        }
+
+
+    }
+}
