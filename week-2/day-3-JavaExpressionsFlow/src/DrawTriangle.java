835f6752c518e0accae9b623830e71fe48904305
Zdenek L <luzkix@seznam.cz>
2020-07-26 12:28:02 +0200

    Added DrawTriangle excercise

--- week-2/day-3-JavaExpressionsFlow/src/DrawTriangle.java
+++ week-2/day-3-JavaExpressionsFlow/src/DrawTriangle.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class DrawTriangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give me a number:");
+        int numberOfLines = scanner.nextInt();
+
+        for (int linenumber = 1; linenumber <= numberOfLines; linenumber++) {
+            System.out.println();
+            for (int numberOfStar = 1; numberOfStar <= linenumber; numberOfStar++) {
+                System.out.print("*");
+            }
+        }
+
+    }
 }
