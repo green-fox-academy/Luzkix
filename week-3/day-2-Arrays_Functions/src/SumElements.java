159a7d7b8973f10a458eb42cd2868a2a0ddf1fc3
Zdenek L <luzkix@seznam.cz>
2020-07-28 12:28:22 +0200

    SumElements excercise

---
+++ week-3/day-2-Arrays_Functions/src/SumElements.java
@@ -0,0 +1,6 @@
+public class SumElements {
+    public static void main(String[] args) {
+        int[] sumArray = {54, 23, 66, 12};
+        System.out.println(sumArray[1] + sumArray[2]);
+    }
+}
