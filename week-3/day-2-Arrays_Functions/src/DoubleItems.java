71a4f1c84520939337812d0dab173a9cdf679a7e
Zdenek L <luzkix@seznam.cz>
2020-07-28 23:56:16 +0200

    Added excercise DoubleItems

---
+++ week-3/day-2-Arrays_Functions/src/DoubleItems.java
@@ -0,0 +1,11 @@
+public class DoubleItems {
+    public static void main(String[] args) {
+        int[] numList = {3, 4, 5, 6, 7};
+
+        //použil jsem forEach - zde nepoužívám [] a ve funkci ani název matice
+        for (int row : numList) {
+            row = row*2;
+            System.out.print(row + " ");
+        }
+    }
+}
