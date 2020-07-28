499f155667b45be961b1e4f597532a4e35024d59
Zdenek L <luzkix@seznam.cz>
2020-07-28 16:52:06 +0200

    updated incrementElement - added variant 2, however not functional as planned

--- week-3/day-2-Arrays_Functions/src/incrementElement.java
+++ week-3/day-2-Arrays_Functions/src/incrementElement.java
@@ -1,18 +1,26 @@
 public class incrementElement {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5};
-
+        int fixed = numbers[2];
 //Var1: Výsledek je 4,5,6,10,11 z důvodu, že v kroku 3 se změní proměnná numbers[2] z 3 na 6, a dále se přičítá 6
-//Var2: ?? Mohu napravit tím, že vložím novou proměnnou, která na počátku vyčte hodnotu numbers[2] a dále se již nezmění???
+//Var2: ?? Mohu napravit tím, že vložím novou proměnnou, která na počátku vyčte hodnotu numbers[2] - čili 3, a dále se již nezmění???
 
 //Var1:
         for (int i = 0; i < numbers.length; i++) {
             numbers[i]= numbers[i] + numbers[2];
             System.out.println(numbers[i]);
         }
-
         System.out.println();
-        System.out.println(numbers[2]);
+        System.out.println("The output of numbers[2] in Var1 is: " + numbers[2]);
+        System.out.println();
+
+//Var2: !!NEFUNKČNÍ!! tato varianta chtěla zachovat fixní přírůstek o 3, čili používá proměnnou fixed definovanou zpočátku. Ale nefunguje to takto :-(
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i]= numbers[i] + fixed;
+            System.out.println(numbers[i]);
+        }
+        System.out.println();
+        System.out.println("The output of numbers[2] in Var2 is: " + numbers[2]);
     }
 }
 
