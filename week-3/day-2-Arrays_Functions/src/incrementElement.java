ab4e1619a4cba9bbf9263ab3746fe8315a16f18b
Zdenek L <luzkix@seznam.cz>
2020-07-30 10:45:10 +0200

    Added excercise AppendAFunc

--- week-3/day-2-Arrays_Functions/src/incrementElement.java
+++ week-3/day-2-Arrays_Functions/src/incrementElement.java
@@ -1,23 +1,28 @@
 public class incrementElement {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5};
-        int fixed = numbers[2];
-//Var1: Výsledek je 4,5,6,10,11 z důvodu, že v kroku 3 se změní proměnná numbers[2] z 3 na 6, a dále se přičítá 6
-//Var2: ?? Mohu napravit tím, že vložím novou proměnnou, která na počátku vyčte hodnotu numbers[2] - čili 3, a dále se již nezmění???
+/*The task was to "- Increment the third element", i.e. to take [3] = 3 and in my understanding to add it to all elements:
 
-//Var1:
-        for (int i = 0; i < numbers.length; i++) {
-            numbers[i]= numbers[i] + numbers[2];
-            System.out.println(numbers[i]);
+Var1: using for loop and adding value of [3] to all elements. Result, however, will be 4,5,6,10,11 from the reason, that
+during loop the value of [3] changes from 3 into 6, meaning 6 is then added instead of 3 since 4th element.
+
+Var2: The value of third element stays fixed (i.e. it stays unchanged during loops) by adding a new variable "fixed",
+which reads out the value of 3rd element at beginning (=3) and stays unchanged */
+        int fixed = numbers[2];
+
+//Var1: simple adding of 3rd element (3) which however changes during third loop into 6 (3+3=6)
+        for (int elementIndex = 0; elementIndex < numbers.length; elementIndex++) {
+            numbers[elementIndex]= numbers[elementIndex] + numbers[2];
+            System.out.println(numbers[elementIndex]);
         }
         System.out.println();
         System.out.println("The output of numbers[2] in Var1 is: " + numbers[2]);
         System.out.println();
 
-//Var2: !!NEFUNKČNÍ!! tato varianta chtěla zachovat fixní přírůstek o 3, čili používá proměnnou fixed definovanou zpočátku. Ale nefunguje to takto :-(
-        for (int i = 0; i < numbers.length; i++) {
-            numbers[i]= numbers[i] + fixed;
-            System.out.println(numbers[i]);
+//Var2: Created a new variable "fixed" prior using forloop, which fixes the original value of third element (i.e. value 3 stays unchanged)
+        for (int elementIndex = 0; elementIndex < numbers.length; elementIndex++) {
+            numbers[elementIndex]= numbers[elementIndex] + fixed;
+            System.out.println(numbers[elementIndex]);
         }
         System.out.println();
         System.out.println("The output of numbers[2] in Var2 is: " + numbers[2]);
