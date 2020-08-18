fd7d98697d848c654da8238480f465350251d54a
Zdenek L <luzkix@seznam.cz>
2020-08-18 15:40:39 +0200

    Teacher - student exercise

---
+++ week-5/day-3-objects/src/TeacherStudent/TeacherStudentApp.java
@@ -0,0 +1,10 @@
+package TeacherStudent;
+
+public class TeacherStudentApp {
+  public static void main(String[] args) {
+    Student student = new Student();
+    Teacher teacher = new Teacher();
+    student.question(teacher);
+    teacher.teach(student);
+  }
+}
