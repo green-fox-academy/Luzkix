bf989dc13a26a36188bc60c0d211c95d7b6a89a9
Zdenek L <luzkix@seznam.cz>
2020-08-29 16:00:02 +0200

    Bunnies again

---
+++ week-6/day-3-recursions/src/BunniesAgain.java
@@ -0,0 +1,23 @@
+public class BunniesAgain {
+  /*We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies (1, 3, ..)
+  have the normal 2 ears. The even bunnies (2, 4, ..) we'll say have 3 ears,
+  because they each have a raised foot. Recursively return the number of "ears"
+  in the bunny line 1, 2, ... n (without loops or multiplication).*/
+
+  public static void main(String[] args) {
+    System.out.println(bunnies(10));
+  }
+
+  private static int bunnies(int bunny) {
+    if (bunny == 1) {
+      return 2;
+    } else {
+      if (bunny%2 == 0) {
+        return 3 + bunnies(bunny - 1);
+      } else {
+        return 2 + bunnies(bunny - 1);
+      }
+    }
+  }
+
+}
