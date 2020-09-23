09d287136ef671c705f9d4c3c03049c64d4300af
Zdenek L <luzkix@seznam.cz>
2020-09-23 22:19:33 +0200

    read file + count the words

--- Test-directory/src/string/justTest.java
+++ Test-directory/src/string/justTest.java
@@ -1,14 +1,31 @@
 package string;
 
-import java.sql.SQLOutput;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 public class justTest {
   public static void main(String[] args) {
 
-    //Př.1 Kolik má věta slov? - převedu na slova dle mezer a spočtu
-    String testString = "ahoj, jak se máš? Mám se dobře, a ty?";
+
+    String testString2 = "ahoj, jak se máš? Mám se dobře, a ty? ";
+    String[] splitSentence = testString2.split(", ");
+    System.out.println(splitSentence[1]);
+
+    //Př.1 Kolik má soubor slov? - převedu na slova dle mezer a spočtu
+    readFile();
+    String testString = "";
+
+    for (String line : readFile()) {
+      testString += line;
+    }
+
+    String[] splitWords = testString.split(" ");
+    System.out.println("The book has " + splitWords.length + " split words!");
+
     List<String> testList = new ArrayList<>();
 
       while (testString.length() != 0){
@@ -23,13 +40,21 @@ public class justTest {
       }
     int numberOfWords = testList.size();
 
-    System.out.println(testString);
-    System.out.println(testList);
+    //System.out.println(testString);
+    //tem.out.println(testList);
     System.out.println("The sentence has " + numberOfWords + " words!");
 
   }
 
-
+  private static List<String> readFile() {
+    try {
+      Path path = Paths.get("src/string/files/The_Hunger_Games.txt");
+      List<String> lines = Files.readAllLines(path);
+      return lines;
+    } catch (IOException e) {
+      throw new RuntimeException("Source file is not accessible!");
+    }
+  }
 
 
 }
