7193cbb1c8dd0945659020a721793d42a07cd1dd
Zdenek L <luzkix@seznam.cz>
2020-10-06 11:41:03 +0200

    Exercise 8

---
+++ week-13/day-1-Streams/src/Ex08.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Ex08 {
+  public static void main(String[] args) {
+    //Write a Stream Expression to concatenate a Character list to a string!
+    List<Character> individualLetters = Arrays.asList('a', 'b','C',' ','a','h','o','j');
+    String sentence = individualLetters.stream()
+        .map(a -> a.toString())
+        .reduce((a , b) -> a.concat(b))//nebo .reduce(String::concat);
+        .get(); //list typu Optional přemění na String
+
+    System.out.println(sentence);
+
+  }
+}
