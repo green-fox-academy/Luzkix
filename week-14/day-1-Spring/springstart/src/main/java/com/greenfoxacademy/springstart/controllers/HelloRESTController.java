c9b53720fbb6ae5a4449db2ed35d77664ad82e1a
Zdenek L <luzkix@seznam.cz>
2020-10-13 19:02:30 +0200

    Hello World Web App

--- week-14/day-1-Spring/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-14/day-1-Spring/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -1,14 +1,17 @@
 package com.greenfoxacademy.springstart.controllers;
 
+import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRESTController {
+  AtomicLong atomicLong = new AtomicLong();
 
-  @GetMapping ("/greeting")
-  public Greeting greeting () {
-    return new Greeting(1,"Hello, World!");
+  @GetMapping ({"/greeting", "/ahoj"}) //mohu zadat více linků
+  public Greeting greeting (@RequestParam String name) {
+    return new Greeting(atomicLong.incrementAndGet(),"Hello, " + name);
   }
 
 }
