92bba846904b2b12808ac2b67f16b8c6a37bc93f
Zdenek L <luzkix@seznam.cz>
2021-01-14 23:41:21 +0100

    Few integration tests implemented

--- week-26_27_projectPhase_GIT_Security/ToDoAdvanced/src/test/java/com/example/todo/IntegrationTodoServiceTests.java
+++ week-26_27_projectPhase_GIT_Security/ToDoAdvanced/src/test/java/com/example/todo/IntegrationTodoServiceTests.java
@@ -1,6 +1,8 @@
 package com.example.todo;
 
+import static org.hamcrest.Matchers.*;
 import static org.hamcrest.core.Is.is;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -13,7 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.http.HttpHeaders;
+
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -30,17 +32,23 @@ public class IntegrationTodoServiceTests {
   private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
       MediaType.APPLICATION_JSON.getSubtype());
 
-
   @Test
   public void createNewCorrectTodo_ReturnsCreatedTodo() throws Exception {
     String inputJson = "{\"title\":\"test\",\"content\":\"test content\",\"urgent\":true}";
 
+    //mocking of Principal, which delivers username
     Principal mockPrincipal = Mockito.mock(Principal.class);
     Mockito.when(mockPrincipal.getName()).thenReturn("petr");
 
+    /*//!!!Not working: mocking of 1 particular method in case the database is not available/is empty:
+    UserService userService = Mockito.spy(UserService.class);
+    Role role = new Role();
+    TodoOwner ownerPetr = new TodoOwner("petr", "heslo", role);
+    ownerPetr.setId(1l);
+    Mockito.doReturn(ownerPetr).when(userService).findOwnerBasedOnUserName("petr");
+*/
     mockMvc.perform(post("/newTodo")
         .principal(mockPrincipal)
-        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
         .contentType(MediaType.APPLICATION_JSON)
         .content(inputJson))
 
@@ -49,8 +57,78 @@ public class IntegrationTodoServiceTests {
         .andExpect(content().contentType(contentType))
         .andExpect(jsonPath("$.title", is("test")))
         .andExpect(jsonPath("$.content", is("test content")))
-        .andExpect(jsonPath("$.urgent", is(true)));
-        //.andExpect(jsonPath("$.result[3]", is(20)));
+        .andExpect(jsonPath("$.urgent", is(true)))
+        .andExpect(jsonPath("$.owner.login",is("petr")));
   }
 
+  @Test
+  public void filter_Urgent_ShouldReturnUrgentTodos()
+      throws Exception {
+    //preparation of input json
+    String inputJson = "{\"method\":\"filter\",\"filterBy\":\"urgent\",\"urgent\":true}";
+
+    mockMvc.perform(get("/listTodos")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(inputJson))
+
+        .andDo(MockMvcResultHandlers.print())
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(contentType))
+        .andExpect(jsonPath("$.filteredTodos.*", hasSize(5)));
+  }
+
+  @Test
+  public void filterUrgent_urgentStatusNotFilled_ShouldReturn400andErrorDTO()
+      throws Exception {
+    //preparation of input json
+    String inputJson = "{\"method\":\"filter\",\"filterBy\":\"urgent\"}";
+
+    mockMvc.perform(get("/listTodos")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(inputJson))
+
+        .andDo(MockMvcResultHandlers.print())
+        .andExpect(status().isBadRequest())
+        .andExpect(content().contentType(contentType))
+        .andExpect(jsonPath("$.error", is("Please provide the json for filtering Todos with correct data!")));
+  }
+
+  @Test
+  public void updateTodoShouldReturnUpdatedTodo() throws Exception{
+    //preparation of input json
+    String inputJson = "{\"id\":\"1\",\"title\":\"new title\"}";
+
+    //mocking of Principal, which delivers username
+    Principal mockPrincipal = Mockito.mock(Principal.class);
+    Mockito.when(mockPrincipal.getName()).thenReturn("zdenek");
+
+    mockMvc.perform(put("/updateTodo")
+        .principal(mockPrincipal)
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(inputJson))
+        .andDo(MockMvcResultHandlers.print())
+        .andExpect(status().is(200))
+        .andExpect(content().contentType(contentType))
+        .andExpect(jsonPath("$.id", is(1)))
+        .andExpect(jsonPath("$.title", is("new title")));
+  }
+
+  @Test
+  public void updateTodoWithWrongUserShouldReturn401AndErrorDTO() throws Exception{
+    //preparation of input json
+    String inputJson = "{\"id\":\"1\",\"title\":\"new title\"}";
+
+    //mocking of Principal, which delivers username
+    Principal mockPrincipal = Mockito.mock(Principal.class);
+    Mockito.when(mockPrincipal.getName()).thenReturn("petr");
+
+    mockMvc.perform(put("/updateTodo")
+        .principal(mockPrincipal)
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(inputJson))
+        .andDo(MockMvcResultHandlers.print())
+        .andExpect(status().is(401))
+        .andExpect(content().contentType(contentType))
+        .andExpect(jsonPath("$.error", is("You are not the Owner of the Todo you want to change - Todo was not updated")));
+  }
 }
---
+++ week-26_27_projectPhase_GIT_Security/ToDoAdvanced/src/test/resources/data.sql
@@ -0,0 +1,20 @@
+
+INSERT INTO role_table (id, name) VALUES (1, 'ROLE_ADMIN');
+INSERT INTO role_table (id, name) VALUES (2, 'ROLE_USER');
+
+INSERT INTO todo_owner (id, password, login, role_id) VALUES (7, '$2a$10$foepraoNZXTt974aygcvkei6TRM..izWmu3deoZBlcpXpHwycWAPy', 'zdenek', 1);
+INSERT INTO todo_owner (id, password, login, role_id) VALUES (2, '$2a$10$HYV0yjstVQR.c0EQUQY4S.8AJNkyUzl87F3E1VVw6aeY5d2G6nbC.', 'luzki', 1);
+INSERT INTO todo_owner (id, password, login, role_id) VALUES (4, '$2a$10$Ip1CwmbBWUNANskmoqahrOhou/g/AWjLEo3UYyysNYgyuCjzAvd7m', 'luzkix', 2);
+INSERT INTO todo_owner (id, password, login, role_id) VALUES (5, '$2a$10$7nt6YMWmX9mnduJKaxr.gOLlus1WbdkIRxrPx6ZV/Yn.eTB/q60va', 'petr', 2);
+INSERT INTO todo_owner (id, password, login, role_id) VALUES (6, '$2a$10$ITJl0lrKQ7.5RmIKtiZDIeHwlFL0rxoZx5NpKeY5KPexRWrEf/dCC', 'ahmed', 2);
+
+INSERT INTO todo (id, content, creation_date, due_date, title, urgent, completed, completition_date, owner_id) VALUES (1, 'Reinstall Windows', '2021-01-05', '2021-05-04', 'Repair PC', false, null, null, 7);
+INSERT INTO todo (id, content, creation_date, due_date, title, urgent, completed, completition_date, owner_id) VALUES (2, 'Buy big house with 3 floors for good price', '2021-01-05', '2021-08-20', 'Buy house', true, null, null, 5);
+INSERT INTO todo (id, content, creation_date, due_date, title, urgent, completed, completition_date, owner_id) VALUES (3, 'Call your mother', '2021-01-05', '2021-01-20', 'Call home', false, null, null, 2);
+INSERT INTO todo (id, content, creation_date, due_date, title, urgent, completed, completition_date, owner_id) VALUES (4, 'Marry christmas letter to Jack', '2021-01-05', null, 'Send a letter', false, true, '2021-01-05', 5);
+INSERT INTO todo (id, content, creation_date, due_date, title, urgent, completed, completition_date, owner_id) VALUES (26, 'Marry christmas letter to Jack', '2021-01-06', '2020-12-31', 'Send a letter', true, false, null, 4);
+INSERT INTO todo (id, content, creation_date, due_date, title, urgent, completed, completition_date, owner_id) VALUES (27, 'Buy big house with 3 floors for good price', '2021-01-06', '2021-08-20', 'Buy house', true, false, null, 6);
+INSERT INTO todo (id, content, creation_date, due_date, title, urgent, completed, completition_date, owner_id) VALUES (28, 'Buy big house with 3 floors for good price', '2021-01-06', '2021-08-20', 'Buy house', true, true, '2021-01-08', 6);
+INSERT INTO todo (id, content, creation_date, due_date, title, urgent, completed, completition_date, owner_id) VALUES (30, 'Reinstall Windows and Office', '2021-01-06', '2021-06-22', 'Repair PC', true, true, '2021-01-05', 2);
+INSERT INTO todo (id, content, creation_date, due_date, title, urgent, completed, completition_date, owner_id) VALUES (33, 'Reinstall Windows', '2021-01-06', null, 'updated title - Reinstall PC2', false, false, null, 7);
+INSERT INTO todo (id, content, creation_date, due_date, title, urgent, completed, completition_date, owner_id) VALUES (34, 'Call your brother', '2021-01-06', '2021-01-20', 'Call brother', false, true, '2020-12-31', 4);
